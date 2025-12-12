3519eb803db7af77eb2def8216503c0b49d54fce
Hüseyn Tahirov <huseyntahirov2@gmail.com>
2025-12-12 08:47:44 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,16 @@
 
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCEvNr8jf5wm5_bwCQotVJWLGrJi136YeY",
+  authDomain: "bioscriptsayt.firebaseapp.com",
+  projectId: "bioscriptsayt",
+  storageBucket: "bioscriptsayt.appspot.com",
+  messagingSenderId: "398300054604",
+  appId: "1:398300054604:web:67f2c4253ce42e0c26e0e9",
+  measurementId: "G-W3790RE4TD"
 };
 
 
@@ -18,6 +20,15 @@ if (!getApps().length) {
   app = initializeApp(firebaseConfig);
 } else {
   app = getApps()[0];
+}
+
+// Initialize Analytics if supported
+if (typeof window !== 'undefined') {
+  isSupported().then(supported => {
+    if (supported) {
+      getAnalytics(app);
+    }
+  });
 }
 
 const db = getFirestore(app);
